5a2232244f3dfcffc4ef815b5ae14fd4adde783a
Koichiro IWAO <meta@vmeta.jp>
2018-08-27 16:40:09 +0900

    build with distro's pulseaudio source

--- .travis.centos.sh
+++ .travis.centos.sh
@@ -4,18 +4,24 @@ SRC_DIR=${PWD}
 
 cd /tmp
 yum install -y epel-release
-yum install -y xrdp xrdp-devel xrdp-selinux wget
+yum install -y xrdp xrdp-devel xrdp-selinux wget sudo
 yum install -y pulseaudio pulseaudio-libs pulseaudio-libs-devel
+yum install -y rpmdevtools yum-utils
 yum-builddep -y pulseaudio
 yum groupinstall -y "Development Tools"
 
-PULSE_VER=$(pkg-config --modversion libpulse)
+sed -i.bak \
+  -e 's/\(^%wheel\s*ALL=(ALL)\s*ALL\)/# \1/' \
+  -e 's/^#\s\(%wheel\s*ALL=(ALL)\s*NOPASSWD:\s*ALL\)/\1/' \
+  /etc/sudoers
+useradd -m -G wheel travis
+# Docker issue #2259
+chown -R travis:travis ~travis
 
-# not to make traffic on upstream server
-wget http://distcache.freebsd.org/ports-distfiles/pulseaudio-${PULSE_VER}.tar.xz
-tar xf pulseaudio-${PULSE_VER}.tar.xz
-cd pulseaudio-${PULSE_VER}
-./configure || exit 1
+PULSE_VER=$(pkg-config --modversion libpulse)
+sudo -u travis yumdownloader --source pulseaudio || exit 1
+sudo -u travis rpm  --install pulseaudio\*.src.rpm || exit 1
+sudo -u travis rpmbuild -bb --noclean ~travis/rpmbuild/SPECS/pulseaudio.spec || exit 1
 
 cd ${SRC_DIR}
-./bootstrap && ./configure PULSE_DIR=/tmp/pulseaudio-${PULSE_VER} && make
+sudo -u travis ./bootstrap && ./configure PULSE_DIR=~travis/rpmbuild/BUILD/pulseaudio-${PULSE_VER} && make || exit 1
