e0a3fc3935de39e223f4c3d3788b8c17be507107
Koichiro IWAO <meta@vmeta.jp>
2021-01-04 15:31:14 +0900

    Switch LICENSE to LGPLv2.1
    Closes: #36

--- src/module-xrdp-sink.c
+++ src/module-xrdp-sink.c
@@ -1,25 +1,25 @@
-/**
- * xrdp: A Remote Desktop Protocol server.
- * pulse sink
- *
- * Copyright (C) Jay Sorg 2013
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+/***
+  pulse sink module for xrdp
 
-/*
- * see pulse-notes.txt
-*/
+  Copyright 2004-2008 Lennart Poettering
+  Copyright (C) 2008 Nokia Corporation and/or its subsidiary(-ies).
+  Copyright (C) 2013-2021 Jay Sorg, Neutrino Labs, and all contributors.
+
+  PulseAudio is free software; you can redistribute it and/or modify
+  it under the terms of the GNU Lesser General Public License as published
+  by the Free Software Foundation; either version 2 of the License,
+  or (at your option) any later version.
+
+  PulseAudio is distributed in the hope that it will be useful, but
+  WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+  General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public License
+  along with PulseAudio; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+  USA.
+***/
 
 // config.h from pulseaudio sources
 #ifdef HAVE_CONFIG_H
